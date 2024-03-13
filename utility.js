6f1490a48ba99ce286a7aab10d4263e5ac59449c
oiver55 <osaintilien55@gmail.com>
2024-03-13 15:51:26 -0400

    added more detailed error logs

--- utility.js
+++ utility.js
@@ -419,7 +419,7 @@ function chargeCreditCard(data, callback) {
       if (response.getMessages().getResultCode() == APIContracts.MessageTypeEnum.OK) {
         if (response.getTransactionResponse().getMessages() != null) {
           emailRecipt(data, "Credit Card", response.transactionResponse)
-          console.log(data, "Credit Card", response)
+          // console.log(data, "Credit Card", response)
           // console.log('Successfully created transaction with Transaction ID: ' + response.getTransactionResponse().getTransId());
           // console.log('Response Code: ' + response.getTransactionResponse().getResponseCode());
           // console.log('Message Code: ' + response.getTransactionResponse().getMessages().getMessage()[0].getCode());
